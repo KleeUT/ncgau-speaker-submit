6adf58fe6e4d279dc94d1a3e8a992e44c0e46a8f
Klee Thomas <klee.ut@gmail.com>
2022-07-06 18:49:54 +1000

    :sparkle: cr

--- functions/api/talkSubmission.ts
+++ functions/api/talkSubmission.ts
@@ -1,19 +1,23 @@
 import { PostSubmission, Submission } from "../../types";
 
-export const onRequestGet: PagesFunction = () => {
-  const data: Submission[] = [
-    {
-      speakerEmail: "email@email.com",
-      speakerName: "speker",
-      month: "may",
-      title: "the thing",
-      id: "fake 1",
-    },
-  ];
+export const onRequestGet: PagesFunction<{
+  SPEAKER_SUBMIT_STORE: KVNamespace;
+}> = async ({ env }) => {
+  const keys = await env.SPEAKER_SUBMIT_STORE.list();
+  const itemsPromise = keys.keys.map(async (key) => {
+    const item = await env.SPEAKER_SUBMIT_STORE.get(key.name);
+    return JSON.parse(item);
+  });
+
+  const data: Submission[] = await Promise.all(itemsPromise);
   return new Response(JSON.stringify({ data }));
 };
 
-export const onRequestPost: PagesFunction = async ({ request }) => {
+export const onRequestPost: PagesFunction<{
+  SPEAKER_SUBMIT_STORE: KVNamespace;
+}> = async ({ request, env }) => {
   const body: PostSubmission = await request.json();
+  const id = crypto.randomUUID();
+  await env.SPEAKER_SUBMIT_STORE.put(id, JSON.stringify({ ...body, id }));
   return new Response(JSON.stringify({ data: body.data }));
 };
